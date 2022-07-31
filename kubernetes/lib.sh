b7f2bffcf8253ad5e8a204c3302869ee26ddcd92
Martín Montes <martin11lrx@gmail.com>
2022-07-31 22:47:03 +0200

    Dynamically creating cluster address

---
+++ kubernetes/lib.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -e
+
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+HOSTNAME=$(hostname)
+STATEFULSET_INDEX=${HOSTNAME##*-}
+STATEFULSET_NAME=${HOSTNAME%-*}
+
+is_initialized() {
+  [ ! -z "$(ls -A /var/lib/mysql)" ]
+}
+
+get_new_cluster() {
+  WSREP_NEW_CLUSTER="OFF"
+  if [ "$STATEFULSET_INDEX" = "0" ] && ! $(is_initialized); then
+      WSREP_NEW_CLUSTER="ON"
+  fi
+  if [ "$BOOSTRAP_CLUSTER" ]; then
+      WSREP_NEW_CLUSTER="ON"
+  fi
+  echo $WSREP_NEW_CLUSTER
+}
+
+get_statefulset() {
+  STATEFULSET_URL="https://kubernetes/apis/apps/v1/namespaces/$POD_NAMESPACE/statefulsets/$STATEFULSET_NAME"
+  echo $(curl "$STATEFULSET_URL" -H "Authorization: Bearer $TOKEN" --cacert "$CA_CERT")
+}
+
+get_all_pods_cluster_address() {
+  STATEFULSET=$(get_statefulset)
+  REPLICAS=$(echo $STATEFULSET | jq -r '.spec.replicas')
+  HEADLESS_SERVICE=$(echo $STATEFULSET | jq -r '.spec.serviceName')
+
+  GALERA_CLUSTER_ADDRESS="gcomm://"
+  for (( i=0; i<$REPLICAS; i++ ))
+  do 
+    GALERA_CLUSTER_ADDRESS="$GALERA_CLUSTER_ADDRESS$STATEFULSET_NAME-$i.$HEADLESS_SERVICE.$POD_NAMESPACE.svc.$CLUSTER,"
+  done
+  echo ${GALERA_CLUSTER_ADDRESS%,*}
+}
+
+get_ready_pods_cluster_address() {
+  PODS_URL="https://kubernetes/api/v1/namespaces/$POD_NAMESPACE/pods"
+  PODS=$(
+    curl "$PODS_URL" -H "Authorization: Bearer $TOKEN" --cacert "$CA_CERT" | \
+      jq -r ".items[] | \
+      select(.metadata.name | startswith(\"$STATEFULSET_NAME-\")) | \
+      select(.status.containerStatuses[0].ready == true) | \
+      .metadata.name"
+  )
+  PODS=(
+    $PODS
+  )
+  STATEFULSET=$(get_statefulset)
+  HEADLESS_SERVICE=$(echo $STATEFULSET | jq -r '.spec.serviceName')
+
+  GALERA_CLUSTER_ADDRESS="gcomm://"
+
+  if [[ "${#PODS[@]}" -lt "2" ]]; then
+    echo "$GALERA_CLUSTER_ADDRESS"
+    return
+  fi
+
+  for i in "${PODS[@]}"
+  do 
+    GALERA_CLUSTER_ADDRESS="$GALERA_CLUSTER_ADDRESS$i.$HEADLESS_SERVICE.$POD_NAMESPACE.svc.$CLUSTER,"
+  done
+  GALERA_CLUSTER_ADDRESS="$GALERA_CLUSTER_ADDRESS$HOSTNAME.$HEADLESS_SERVICE.$POD_NAMESPACE.svc.$CLUSTER"
+
+  echo $GALERA_CLUSTER_ADDRESS
+}
+
+get_cluster_address() {
+  if ! $(is_initialized); then 
+    echo $(get_all_pods_cluster_address)
+    return
+  fi
+  echo $(get_ready_pods_cluster_address)
+}
--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -7,29 +7,23 @@ if [ -z "$MARIADB_ROOT_PASSWORD" ]; then
     exit 1
 fi
 
-HOSTNAME=$(hostname)
-STATEFULSET_INDEX=${HOSTNAME##*-}
+CURRENT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+
+source "$CURRENT_DIR/lib.sh"
+
+# Avoid having a custom Docker image and use the oficial one.
+# TODO: use a sidecar for reloading the config file and remove this
+apt update && apt install -y curl jq
+
 GALERA_CONFIG_FILE=/etc/mysql/mariadb.conf.d/galera.cnf
 GALERA_STATE_FILE=/var/lib/mysql/grastate.dat
 
 if [ -z "$ENTRYPOINT" ]; then 
     ENTRYPOINT="/usr/local/bin/docker-entrypoint.sh"
 fi
-
-WSREP_NEW_CLUSTER="OFF"
-if [ "$STATEFULSET_INDEX" = "0" ] && [ -z "$(ls -A /var/lib/mysql)"  ]; then
-    WSREP_NEW_CLUSTER="ON"
-fi
-if [ "$BOOSTRAP_CLUSTER" ]; then
-    WSREP_NEW_CLUSTER="ON"
-fi
-
 if [ -z "$SAFE_TO_BOOTSTRAP" ]; then 
     SAFE_TO_BOOTSTRAP=true
 fi
-
-GALERA_CLUSTER_ADDRESS="gcomm://mariadb-0.mariadb-headless.default.svc.cluster.local,mariadb-1.mariadb-headless.default.svc.cluster.local,mariadb-2.mariadb-headless.default.svc.cluster.local"
-# GALERA_CLUSTER_ADDRESS="gcomm://"
 
 # MariaDB Galera config file
 cat <<EOF | tee ${GALERA_CONFIG_FILE} 
@@ -40,10 +34,10 @@ binlog_format=row
 innodb_autoinc_lock_mode=2
 
 # Cluster configuration: https://galeracluster.com/library/documentation/mysql-wsrep-options.html
-wsrep_new_cluster="$WSREP_NEW_CLUSTER"
+wsrep_new_cluster="$(get_new_cluster)"
 wsrep_on=ON
 wsrep_provider=/usr/lib/galera/libgalera_smm.so
-wsrep_cluster_address="$GALERA_CLUSTER_ADDRESS"
+wsrep_cluster_address="$(get_cluster_address)"
 wsrep_cluster_name=galera
 wsrep_slave_threads=4
 
@@ -54,7 +48,6 @@ wsrep_node_name="$HOSTNAME"
 # SST: https://mariadb.com/kb/en/introduction-to-state-snapshot-transfers-ssts/ 
 wsrep_sst_method="mariabackup"
 wsrep_sst_auth="root:$MARIADB_ROOT_PASSWORD"
-# wsrep_sst_method="rsync"
 EOF
 
 if [ "$SAFE_TO_BOOTSTRAP" ] && [ -f "$GALERA_STATE_FILE" ]; then 
