e79966178f7f5501971d47da8dc1b4c8ef54bee6
Martin Montes <martin11lrx@gmail.com>
2023-03-19 13:19:10 +0100

    Added primary and readonly users

--- async-repl/primary.sql
+++ async-repl/primary.sql
@@ -1,3 +1,10 @@
-CREATE USER 'repluser'@'%' IDENTIFIED BY 'replsecret';
-GRANT REPLICATION REPLICA ON *.* TO 'repluser'@'%';
+CREATE USER 'repl'@'%' IDENTIFIED BY 'repl';
+GRANT REPLICATION REPLICA ON *.* TO 'repl'@'%';
+
+CREATE USER 'primary'@'%' IDENTIFIED BY 'primary';
+GRANT ALL PRIVILEGES ON *.* TO 'primary'@'%';
+
+CREATE USER 'readonly'@'%' IDENTIFIED BY 'readonly';
+GRANT SELECT ON *.* TO 'readonly'@'%';
+
 CREATE DATABASE primary_db;
--- async-repl/replica.sql
+++ async-repl/replica.sql
@@ -1,5 +1,5 @@
 CHANGE MASTER TO 
 MASTER_HOST='mariadb-0.mariadb.default.svc.cluster.local',
-MASTER_USER='repluser',
-MASTER_PASSWORD='replsecret',
+MASTER_USER='repl',
+MASTER_PASSWORD='repl',
 MASTER_CONNECT_RETRY=10;
